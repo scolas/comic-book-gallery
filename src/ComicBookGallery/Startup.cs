c61cce50dea25d2f4ca01cfc947c922b17e9cc16
scolas <scolas481@gmail.com>
2018-05-13 17:19:37 -0400

    updated default route

--- src/ComicBookGallery/Startup.cs
+++ src/ComicBookGallery/Startup.cs
@@ -42,7 +42,7 @@ namespace ComicBookGallery
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=ComicBooks}/{action=Index}/{id?}");
             });
         }
     }
